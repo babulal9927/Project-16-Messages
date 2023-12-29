bc021d77b87fff3174c60c6a3e28d7a5a6d93060
babulal9927 <129493914+babulal9927@users.noreply.github.com>
2023-12-29 20:06:33 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+(function () {
+    const form = document.querySelector("#form");
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+  
+      const message = document.querySelector(".message");
+      const value = document.querySelector(".for-invalid-input");
+      const content = document.querySelector(".msg-content");
+  
+      if (message.value == "") {
+        value.classList.add("show");
+        setTimeout(() => {
+          value.classList.remove("show");
+        }, 2000);
+      } else {
+        content.textContent = `"${message.value}" that is your message is delivered❤️😊`;
+        message.value = "";
+      }
+    });
+  })();
+  
